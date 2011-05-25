2934a9d45477bc39c515b0287770eb3f6a203362
approaching236 <vgrato@gmail.com>
2011-05-25 13:05:31 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GowallaTest::Application.config.secret_token = 'b9a0fcfc2a621acaa1b559677062fc603f47cf5dc8f079a401f41a306f684557c32ab80648fee1cd868fb9ac6aaccf38659a5026a2bbfb5fe6c1afa8bd70ab5d'
