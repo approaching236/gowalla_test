7ae7304c681d2c58fb36a3eb86c66b4d376ff681
approaching236 <vgrato@gmail.com>
2011-05-25 19:12:29 -0700

    fiddling with open auth

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ GowallaTest::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match '/oauth/start' => 'oauth#start'
+  match '/oauth/callback' => 'oauth#callback'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
@@ -48,7 +50,7 @@ GowallaTest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "spots#index"
 
   # See how all your routes lay out with "rake routes"
 
