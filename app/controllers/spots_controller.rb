726d352344d382508dd5f76dda9bf1a71dc6972b
approaching236 <vgrato@gmail.com>
2011-05-26 23:33:45 -0700

    all done with some extra

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -4,8 +4,41 @@ class SpotsController < ActionController::Base
   protect_from_forgery
 
   def index
-    @spots = JSON.parse(Net::HTTP.get(URI.parse'http://api.gowalla.com/spots.json?lat=32.899056&lng=-117.199348&radius=500'))
-    logger.info 'sup'
-    logger.info params[:sort]
+    spots_json = JSON.parse(Net::HTTP.get(URI.parse'http://api.gowalla.com/spots.json?lat=32.899056&lng=-117.199348&radius=500'))
+
+    @spots = Array.new
+    spots_json['spots'].each do |spot|
+      if spot.first != "groups"
+        @spots.push spot
+      end
+    end
+
+    if params[:sort] == 'name'
+      if request.referer.include? "name"
+        @spots.sort! { |a,b| b['name'] <=> a['name'] }
+      else
+        @spots.sort! { |a,b| a['name'] <=> b['name'] }
+      end
+    elsif params[:sort] == 'region'
+      @spots.sort! { |a,b| a['region'] <=> b['region'] }
+    elsif params[:sort] == 'checkins'
+      if request.referer.include? "checkins"
+        @spots.sort! { |a,b| b['checkins_count'] <=> a['checkins_count'] }
+      else
+        @spots.sort! { |a,b| a['checkins_count'] <=> b['checkins_count'] }
+      end
+    elsif params[:sort] == 'trending'
+      if request.referer.include? "trending"
+        @spots.sort! { |a,b| b['trending_level'] <=> a['trending_level'] }
+      else
+        @spots.sort! { |a,b| a['trending_level'] <=> b['trending_level'] }
+      end
+    elsif params[:sort] == 'users'
+      if request.referer.include? "users"
+        @spots.sort! { |a,b| b['users_count'] <=> a['users_count'] }
+      else
+        @spots.sort! { |a,b| a['users_count'] <=> b['users_count'] }
+      end
+    end
   end
 end
